43c1cd8fa1a19358f3bde5f261064b8441b54b06
Joel Palmer <joelpalmer@gmail.com>
2018-12-10 09:57:03 -0600

    finish requester

---
+++ zmq-filer-req.js
@@ -0,0 +1,20 @@
+"use strict";
+const zmq = require('zeromq');
+const filename = process.argv[2];
+
+// create request endpoint
+const requester = zmq.socket('req');
+
+// handle replies from the responder
+requester.on('message', data => {
+    const response = JSON.parse(data);
+    console.log('Received Response: ', response);
+});
+
+requester.connect('tcp://localhost:60401');
+
+// send a request for content
+console.log(`Sending request for ${filename}`);
+requester.send(JSON.stringify({
+    path: filename
+}));
