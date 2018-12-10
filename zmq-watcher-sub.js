1622372c67e52ddc7c51048130238316bbb908cc
Joel Palmer <joelpalmer@gmail.com>
2018-12-10 09:02:14 -0600

    finish pub/sub

--- zmq-watcher-sub.js
+++ zmq-watcher-sub.js
@@ -1,1 +1,18 @@
 'use strict';
+const zmq = require('zeromq');
+
+// create subscriber endpoint
+const subscriber = zmq.socket('sub');
+
+// subscribe to all messages
+subscriber.subscribe('');
+
+// handle messages from the publisher
+subscriber.on('message', data => {
+    const message = JSON.parse(data);
+    const date = new Date(message.timestamp);
+    console.log(`File "${message.file}" changed at ${date}`);
+});
+
+// connect to publisher
+subscriber.connect("tcp://localhost:60400");
